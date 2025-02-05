316a63e827382ae622066fe69ead3b76d2274986
Muhammad Adnan yousaf <60261849+adnan1103@users.noreply.github.com>
2025-02-05 04:50:27 +0000

    comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,12 +2,12 @@
 // let score = null; // 0
 // let score = undefined;  // NAN
 // let score = true; // 1
-let score = "adnan"
+let score = "adnan";
 
 // console.log(typeof score)
 // console.log(typeof(score));
 
-let valueInNumber = Number(score); 
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber);
 
@@ -23,9 +23,43 @@ let valueInNumber = Number(score);
 // let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
-
-let someNumber = 33
+let someNumber = 33;
 
 let someString = String(someNumber);
-console.log(someString);
-console.log(typeof someString)
+// console.log(someString);
+// console.log(typeof someString)
+
+//*********************Operations********** *//
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// these are the basics math operations
+// console.log(2*2)
+// console.log(2**3) // 2 power 3
+
+let str1 = "hello";
+let str2 = " adnan";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "3");
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "3");
+
+console.log(+true);
+console.log(+"");
+
+// Postfix
+//x++, the increment operator increments and {returns the value {before incrementing}}.
+let gameCounter = 100;
+let y = gameCounter++; //returns the value {before incrementing}
+console.log(`gameCounter value is ${gameCounter}, y value is ${y}`); // output gameCounter value is 101, y value is 100
+
+// Prefix
+//++x, the increment operator {increments and returns the value after incrementing}..
+let gameCounter2 = 100;
+let y2 = ++gameCounter2; // increments and returns the value after incrementing
+console.log(`gameCounter value is ${gameCounter2}, y value is ${y2}`); // output gameCounter value is 101, y value is 101
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+
+// console.log(null > 0)
+// console.log(null >= 0)
+// console.log(null == 0)
+
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+// == this equality sign check that it equal or not
+// === this is the strict check and check the datatype
+
+console.log("2" === 1);
