f13c762cb91fb38f3df7986ef8c0063162134b97
Muhammad Adnan yousaf <60261849+adnan1103@users.noreply.github.com>
2025-02-09 00:53:13 +0000

    JSONs Info

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -63,13 +63,107 @@ const users = [
   },
 ];
 
+users[1].email;
+// console.log(tinderUser);
 
-users[1].email
-console.log(tinderUser)
+// console.log(Object.keys(tinderUser)); // get keys from object  [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); // get values from object [ '123abc', 'Sammy', false ]
+// console.log(Object.entries(tinderUser)); // get one by one seprate entry in array like  [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-console.log(Object.keys(tinderUser)) // get keys from object  [ 'id', 'name', 'isLoggedIn' ]
-console.log(Object.values(tinderUser)) // get values from object [ '123abc', 'Sammy', false ]
-console.log(Object.entries(tinderUser)) // get one by one seprate entry in array like  [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+// console.log(tinderUser.hasOwnProperty("isLoggedIn")); // true // Before working with value you must check
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')) // true // Before working with value you must check
+const course = {
+  coursename: "Js in Urdu",
+  price: 999,
+  courseInstructor: "AdnanYousaf",
+};
 
+// course.courseInstructor  // this is the fine method but accessing any value we call again and again
+
+// Object Destructring { we can change the name}
+const { courseInstructor: instructor } = course;
+console.log(instructor);
+
+// this is destructuring in React
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "Adnan")
+
+// this is the Json foramte
+// {
+//   "name": "Adnan",
+//   "coursename": "Js in Urdu",
+//   "price": "free"
+// }
+
+// Apis in array formate we get some time { and in one array multiple objects}
+// [{}, {},{}, {}, {}]
+
+
+{
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
