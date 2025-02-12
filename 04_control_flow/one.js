8c7d01ba0a2d99c33154e2b873db8837f07f255e
Muhammad Adnan yousaf <60261849+adnan1103@users.noreply.github.com>
2025-02-12 10:53:46 +0000

    Control flow conditions

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,6 +21,28 @@ const balance = 1000
 
 // if(balance > 500) console.log("test")   // Implicite scope {code run in a one line Then we called implicite scope}
 
-if (balance < 500) {
-console.log()
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
 }
